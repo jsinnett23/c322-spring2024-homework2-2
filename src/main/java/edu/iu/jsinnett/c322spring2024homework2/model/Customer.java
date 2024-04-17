daacab451737f687cd333c1a22983579f2fae5f1
jsinnett23 <sinnett.dt@gmail.com>
2024-04-17 15:41:34 -0400

    homework 8 part 2 backend

---
+++ src/main/java/edu/iu/jsinnett/c322spring2024homework2/model/Customer.java
@@ -0,0 +1,7 @@
+package edu.iu.jsinnett.c322spring2024homework2.model;
+
+public record Customer(String username,
+                       String password,
+                       String email) {
+
+}
