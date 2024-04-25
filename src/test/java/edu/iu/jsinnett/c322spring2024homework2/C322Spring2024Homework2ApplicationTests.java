da2d8461c8215c2de936b2c5b51c5ef8f660c74e
jsinnett23 <sinnett.dt@gmail.com>
2024-04-25 12:53:23 -0400

    take 3

--- src/test/java/edu/iu/jsinnett/c322spring2024homework2/C322Spring2024Homework2ApplicationTests.java
+++ src/test/java/edu/iu/jsinnett/c322spring2024homework2/C322Spring2024Homework2ApplicationTests.java
@@ -1,13 +1,11 @@
-//package edu.iu.jsinnett.c322spring2024homework2;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class C322Spring2024Homework2ApplicationTests {
-//
-//	@Test
-//	void contextLoads() {
-//	}
-//
-//}
+package edu.iu.jsinnett.c322spring2024homework2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class C322Spring2024Homework2ApplicationTests {
+
+	void contextLoads() {
+	}
+
+}
